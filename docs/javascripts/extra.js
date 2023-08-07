6c5ca58c7a1598ffe3e9013399373367afc43584
Mat Chavez <1787040+matchavez@users.noreply.github.com>
2023-08-07 17:46:49 +1200

    .

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,15 +1,15 @@
-<!-- Default Statcounter code for GitHub
-https://matchavez.com -->
-    <><script type="text/javascript">
-        var sc_project=12907738;
-        var sc_invisible=1;
-        var sc_security="9ec2d84c";
-    </script><script type="text/javascript"
-        src="https://www.statcounter.com/counter/counter.js"
-        async></script><noscript><div class="statcounter"><a title="Web Analytics"
-            href="https://statcounter.com/" target="_blank"><img
-                class="statcounter"
-                src="https://c.statcounter.com/12907738/0/9ec2d84c/1/"
-                alt="Web Analytics"
-                referrerPolicy="no-referrer-when-downgrade"></></a></div></noscript>< /></>
-                !-- End of Statcounter Code -->
+<script type="text/javascript">
+    var sc_project=12907738;
+    var sc_invisible=1;
+    var sc_security="9ec2d84c";
+</script>
+
+<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async>
+</script>
+
+<noscript>
+    <div class="statcounter">
+        <a title="Web Analytics" href="https://statcounter.com/" target="_blank">
+        <img class="statcounter" src="https://c.statcounter.com/12907738/0/9ec2d84c/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade"></></a>
+    </div>
+</noscript>
